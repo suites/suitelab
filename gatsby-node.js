ef63293bbb41a213f9036da62e19ce75774fbc77
yoon.homme <woosiks.io@gmail.com>
2023-01-31 21:15:33 +0900

    feat: category가 정상 동작하게 변경

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,11 +3,11 @@ const { createFilePath } = require(`gatsby-source-filesystem`);
 const _ = require(`lodash`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions;
+  const { createPage } = actions;
 
   return graphql(
     `
-      {
+      query {
         allMdx(
           sort: { frontmatter: { date: DESC } }
           limit: 1000
@@ -54,40 +54,6 @@ exports.createPages = ({ graphql, actions }) => {
       });
     });
 
-    // get related Posts(retrive maximum 5 posts for each category)
-    let allRelatedPosts = {};
-    categories.forEach(category => {
-      let categoryPosts = nodes.filter(post => {
-        return post.frontmatter.category === category;
-      });
-      allRelatedPosts[category] = categoryPosts
-        ? categoryPosts.slice(0, 5)
-        : [];
-    });
-
-    // Create blog posts pages.
-    nodes.forEach((node, index) => {
-      // const previous =
-      //   index === posts.length - 1 ? null : posts[index + 1].node;
-      // const next = index === 0 ? null : posts[index - 1].node;
-
-      // setup related posts
-      // get the posts that has same categories.
-      // let relatedPosts = allRelatedPosts[post.frontmatter.category];
-      // // remove myself
-      // relatedPosts = relatedPosts.filter(relatedPost => {
-      //   return !(relatedPost.node.fields.slug === post.frontmatter.slug);
-      // });
-
-      createPage({
-        path: node.frontmatter.slug,
-        component: node.internal.contentFilePath,
-        context: {
-          id: node.id,
-          // relatedPosts
-        }
-      });
-    });
   });
 };
 
--- src/templates/categories.tsx
+++ src/templates/categories.tsx
@@ -6,7 +6,6 @@ import CategoryJsonLd from '../components/json/CategoryJsonLd';
 import Layout from '../components/Layout';
 import PostCard from '../components/PostCard';
 import SEO from '../components/SEO';
-import { QueryResult } from '../models';
 import { CategoryPageContext } from '../models';
 
 const Heading = styled.h1`
@@ -18,41 +17,48 @@ const Heading = styled.h1`
   letter-spacing: 1px;
 `;
 
-interface Props {
-  data: QueryResult;
-  pageContext: CategoryPageContext;
-  location: any;
-}
-class CategoryTemplate extends React.Component<PageProps<Queries.BlogPostByIdQuery, CategoryPageContext>> {
-  render() {
-    return <></>
-    // const { data, pageContext } = this.props;
-    // // const posts = data.allMarkdownRemark.edges;
-    // const { location } = this.props;
-    // // get Category name from category slug
-    // const categorySlug = pageContext.category;
-    // const categories = data.site!!.siteMetadata!!.;
-    // const categoryObject = categories.find((cat) => {
-    //   return cat.slug === categorySlug;
-    // });
-    // // use slug when name doesn't exist
-    // const categoryName = categoryObject ? categoryObject.name : categorySlug;
 
-    // return (
-    //   <Layout location={this.props.location} title={categoryName}>
-    //     <SEO title={categoryName} />
-    //     <CategoryJsonLd
-    //       categorySlug={categorySlug}
-    //       categoryName={categoryName}
-    //     />
-    //     <CategoryMenu location={location} categories={categories} />
-    //     <Heading>{categoryName}</Heading>
-    //     {/* {posts.map(({ node }) => {
-    //       return <PostCard key={node.fields.slug} node={node} />;
-    //     })} */}
-    //   </Layout>
-    // );
-  }
+const CategoryTemplate = ({data, pageContext, location}: PageProps<Queries.BlogPostByCategoryQuery, CategoryPageContext>) => {
+  const posts = data.allMdx.nodes;
+  // get Category name from category slug
+  const categorySlug = pageContext.category;
+  const categories = data.site!!.siteMetadata!!.categories;
+  const categoryObject = categories!!.find((cat) => {
+    return cat!!.slug === categorySlug;
+  });
+  // use slug when name doesn't exist
+  const categoryName = categoryObject ? categoryObject.name : categorySlug;
+
+  return (
+    <Layout location={location} title={categoryName!!}>
+      <SEO title={categoryName!!} />
+      <CategoryJsonLd
+        categorySlug={categorySlug}
+        categoryName={categoryName!!}
+      />
+      <CategoryMenu location={location} categories={categories!!.map((category) => {
+        return {
+          color: category!!.color!!,
+          icon: category!!.icon!!,
+          link: category!!.link!!,
+          name: category!!.name!!,
+          slug: category!!.slug!!
+        }
+      }
+      )} />
+      <Heading>{categoryName}</Heading>
+      {posts.map(({ frontmatter }) => {
+        return <PostCard key={frontmatter!!.slug!!} frontmatter={{
+          category: frontmatter!!.category!!,
+          date: frontmatter!!.date!!,
+          description: "",
+          emoji: frontmatter!!.emoji!!,
+          slug: frontmatter!!.slug!!,
+          title: frontmatter!!.title!!,
+        }} />;
+      })}
+    </Layout>
+  );
 }
 
 export default CategoryTemplate;
